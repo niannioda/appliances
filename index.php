28bb1d00259398ecdeca88820f024315a980cc2e
niannioda <niodajhoshnian@gmail.com>
2026-05-25 14:12:42 +0800

    push

--- index.php
+++ index.php
@@ -345,11 +345,6 @@ $outOfStock = $p['stock'] <= 0;
   }
 
 })();
-</script>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-<script src="assets/js/app.js"></script>
 
 </body>
 </html>

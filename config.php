d4bfcd5676e57723c102a18c42e199e6d81f65ac
niannioda <niodajhoshnian@gmail.com>
2026-05-28 13:42:01 +0800

    da

--- config.php
+++ config.php
@@ -1,11 +1,17 @@
 <?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 define('DB_HOST', 'sql306.infinityfree.com');
 define('DB_USER', 'if0_42036489_XXX');
-define('DB_PASS', 'sethborres18');
+define('DB_PASS', 'your_password');
 define('DB_NAME', 'if0_42036489_appliances_inventory');
 
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-if ($conn->connect_error) {
-    die(json_encode(['error' => 'Connection failed: ' . $conn->connect_error]));
+try {
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $conn->set_charset('utf8mb4');
+
+    echo "Connected successfully";
+
+} catch (mysqli_sql_exception $e) {
+    die("Connection failed: " . $e->getMessage());
 }
-$conn->set_charset('utf8mb4');
